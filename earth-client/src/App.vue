9ff86cbe4b9e78ff29f57fdaac1eaadd2af0bf36
skryuchkov <skryuchkov@gemsdev.com>
2024-04-25 13:22:59 +0600

    empty vue js project

---
+++ earth-client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    hello
+  </header>
+
+  <main>
+    world
+  </main>
+</template>
+
+<style scoped>
+
+</style>
