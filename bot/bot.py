f434a7afe67775e465c7ca997e2d009d37a192be
skryuchkov <skryuchkov@gemsdev.com>
2024-06-09 15:02:03 +0600

    hello world with our app

--- bot/bot.py
+++ bot/bot.py
@@ -1,16 +1,26 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
+from aiogram.types import Message, KeyboardButton, WebAppInfo, InlineKeyboardButton
+from aiogram.utils.keyboard import KeyboardBuilder, InlineKeyboardBuilder
 from dotenv import find_dotenv, load_dotenv
-
-from handlers.user import router
 
 load_dotenv(find_dotenv())
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
+router = Router()
 dp.include_router(router)
+
+kb = InlineKeyboardBuilder()
+
+kb.row(InlineKeyboardButton(text="Run app", web_app=WebAppInfo(url="https://gitlab.gemsdev.ru/gems/geometa/-/pipelines")))
+
+
+@router.message()
+async def handle(message: Message):
+    await message.answer("hello", reply_markup=kb.as_markup())
 
 
 async def main():
